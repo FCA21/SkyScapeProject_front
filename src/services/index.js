e5ae577e6b0b324ccf7c88947413de5dffaf898e
Fernando Castro <fernando_1991_21@hotmail.com>
2024-12-04 10:52:34 +0000

    corrección

--- src/services/index.js
+++ src/services/index.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 
 const api = axios.create( {
-   /*  baseURL: "https://jsonplaceholder.typicode.com", */
    baseURL: "http://localhost:3000/api",
     withCredentials: false,
 });
